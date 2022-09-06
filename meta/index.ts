f92d0b19b5908f978d5366c240379136e8d9d2ef
vaakian <vaakian@gmail.com>
2022-09-06 16:28:32 +0800

    fix(meta): resolve wrong path

--- meta/index.ts
+++ meta/index.ts
@@ -5,7 +5,7 @@ const subPackages = ['core', 'vue', 'angular', 'react', 'web-component']
 const generateSubAlias = () => {
   const entries = subPackages.map(key => [
     `@contextmenu/${key}`,
-    path.resolve(__dirname, `packages/${key}/index.ts`),
+    path.resolve(__dirname, `../packages/${key}/index.ts`),
   ])
   return Object.fromEntries(entries)
 }
