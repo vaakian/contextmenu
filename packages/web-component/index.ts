c713f7b90d6ee77720576016401f55bbd1bd4734
vaakian <vaakian@gmail.com>
2022-09-07 19:28:30 +0800

    feat: web component setup with `lit`

---
+++ packages/web-component/index.ts
@@ -0,0 +1,47 @@
+import { LitElement, css, html } from 'lit'
+import { customElement } from 'lit/decorators'
+
+@customElement('context-menu')
+export class ContextMenu extends LitElement {
+  static styles = css`
+  :host {
+    color: red
+  }
+    `
+  render() {
+    return html`<p>hello, contextMenu!</p>`
+  }
+}
+// // See: https://javascript.info/custom-elements
+// export class ContextMenu extends HTMLElement {
+//   constructor() {
+//     super()
+//     // element created
+//   }
+
+//   connectedCallback() {
+//     // browser calls this method when the element is added to the document
+//     // (can be called many times if an element is repeatedly added/removed)
+//   }
+
+//   disconnectedCallback() {
+//     // browser calls this method when the element is removed from the document
+//     // (can be called many times if an element is repeatedly added/removed)
+//   }
+
+//   static get observedAttributes() {
+//     return [/* array of attribute names to monitor for changes */]
+//   }
+
+//   attributeChangedCallback(name, oldValue, newValue) {
+//     return { name, oldValue, newValue }
+//     // called when one of attributes listed above is modified
+//   }
+
+//   adoptedCallback() {
+//     // called when the element is moved to a new document
+//     // (happens in document.adoptNode, very rarely used)
+//   }
+
+//   // there can be other element methods and properties
+// }
