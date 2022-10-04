30efc0c1b2a0973853e42cbd2a939503278e51a7
vaakian <vaakian@gmail.com>
2022-10-04 19:37:27 +0800

    feat(docs): add demo to usage

---
+++ docs/guide/Menu.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const menus = [
+  ['copy', 'i-carbon-moon'],
+  ['paste', 'i-carbon-bike'],
+  ['back', 'i-carbon-back'],
+]
+</script>
+
+<template>
+  <div
+    class="menu"
+    b="~ stone-500/15"
+    bg-white
+    rounded-md
+    shadow-md
+    overflow-hidden
+    flex
+    flex-col
+    bg="$vp-c-bg-soft"
+    select-none
+    z-20
+  >
+    <!-- title -->
+    <div
+      px-3
+      py="2px"
+      lowercase
+      hover="bg-stone-500/10 cursor-pointer text-$vp-c-brand"
+    >
+      <slot />
+    </div>
+
+    <div
+      v-for="([value], key) in menus"
+      :key="key"
+      px-3
+      py="2px"
+      lowercase
+      text="$vp-c-text-code"
+      hover="bg-stone-500/10 cursor-pointer text-$vp-c-brand"
+    >
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+
+</style>
--- docs/guide/usage.md
+++ docs/guide/usage.md
@@ -32,6 +32,32 @@ const menu = document.getElementById('menu')
 // the default `target` is the global `window`.
 const ctxMenu = createContextMenu(menu)
 ```
+
+
+## Demo
+<script setup>
+import { ref } from 'vue'
+import { useContextMenu } from '@contextmenu/vue'
+import Menu from './Menu.vue'
+
+const targetRef = ref(null)
+const menuRef = ref(null)
+
+const ctx = useContextMenu(menuRef, { target: targetRef })
+console.log(ctx)
+</script>
+
+<div 
+  ref="targetRef"
+  class="w-20 h-20 bg-red-500/20 flex rounded-lg items-center justify-center text-center box-content select-none"
+  p="x-10 y-10"
+  mx-auto
+>
+  right click on me
+</div>
+
+<Menu ref="menuRef" />
+
 ## Framework support
 The description introduces the basic idea and it's used in native javascript, we also provide high level framework preset:
 - [Vue](/vue/)
