d647e55413e99abf17f2b1378ba1f76985cc70e9
vaakian <vaakian@gmail.com>
2022-10-30 21:42:19 +0800

    chore: add build script

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
       {
         text: 'Vue',
         items: [
-          { text: 'Installation', link: '/vue' },
+          { text: 'Installation', link: '/vue/' },
           { text: 'Component', link: '/vue/component' },
           { text: 'Hook', link: '/vue/hook' },
           { text: 'Directive', link: '/vue/directive' },
